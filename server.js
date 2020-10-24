2513ece8ecf912e06180724c0996c1d6801fa79e
wilmerbaby <wilmerbaby@gmail.com>
2020-10-24 13:02:16 -0400

    made get rout for stats

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.get("/", (req, res) => {
  res.sendFile(path.join(_dirname + "./public/index.html"));
 });
 
+app.get("/stats", (req, res) =>{
+  res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
 app.get("/exercise", (req, res) => {
   db.Workout.find({}).populate("exercisee").then(dbUser => {
     res.json(dbUser);
