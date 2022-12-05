e99a2ed1564b521024c121cb66de00f6ca1c1cca
Wilmer Rivera <wilmerbaby@gmail.com>
2022-12-04 23:18:39 -0500

    finished require path

--- routes/workoutController.js
+++ routes/workoutController.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout");
+const Workout = require("../models/Workout.js");
 router.post("/api/workouts", (req, res) => {
   Workout.create({})
     .then(dbWorkout => {
